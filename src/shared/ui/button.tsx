bea3d414bd782a4bb53cca0daeb1671217df21b7
Egor Volg <duna9588@gmail.com>
2025-08-10 19:39:27 +0300

    feat: add props on button component

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,5 +1,17 @@
 import styles from "./Button.module.css";
 
-export default function Button({ children }: { children: React.ReactNode }) {
-  return <div className={styles.button}>{children}</div>;
+export default function Button({
+  children,
+  onClick,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+}) {
+  return (
+    <div className={`${styles.button} ${className}`} onClick={onClick}>
+      {children}
+    </div>
+  );
 }
