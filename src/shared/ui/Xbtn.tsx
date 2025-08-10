3dc01bbcc66fff98c728722aeb85d245a16c36a0
Egor Volg <duna9588@gmail.com>
2025-08-10 19:37:19 +0300

    feat: Add Xbtn component with SVG icon and styling

---
+++ src/shared/ui/Xbtn.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Xbtn.module.css";
+
+const Xbtn = ({
+  onClick,
+  className,
+}: {
+  onClick: () => void;
+  className?: string;
+}) => {
+  return (
+    <button className={`${styles.xbtn} ${className}`} onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="30px"
+        viewBox="0 -960 960 960"
+        width="30px"
+        fill="#000"
+      >
+        <path d="m336-280-56-56 144-144-144-143 56-56 144 144 143-144 56 56-144 143 144 144-56 56-143-144-144 144Z" />
+      </svg>
+    </button>
+  );
+};
+
+export default Xbtn;
