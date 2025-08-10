4c98ab0edb59385f7aa6ba20ff3809b17683a397
Egor Volg <duna9588@gmail.com>
2025-08-11 02:54:05 +0300

    fix: fix some styles for adaptive

--- src/shared/components/Card.tsx
+++ src/shared/components/Card.tsx
@@ -8,19 +8,18 @@ export const Card = ({ pizza }: { pizza: Pizza }) => {
       <div className={styles.imageContainer}>
         <img className={styles.image} src={pizzaImage} alt={pizza.name} />
       </div>
-      
+
       <div className={styles.content}>
         <h3 className={styles.title}>{pizza.name}</h3>
         <p className={styles.ingredients}>{pizza.description}</p>
+      </div>
+      <div className={styles.priceContainer}>
+        <span className={styles.price}>
+          от <b className={styles.priceBold}>{pizza.price} ₽</b>
+        </span>
 
-        
-      </div><div className={styles.priceContainer}>
-          <span className={styles.price}>
-            от <b className={styles.priceBold}>{pizza.price} ₽</b>
-          </span>
-
-          <button className={styles.button}>+ Добавить</button>
-        </div>
+        <button className={styles.button}>+ Добавить</button>
+      </div>
     </div>
   );
 };
