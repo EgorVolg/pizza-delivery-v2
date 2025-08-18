35110c79151f85cfd0712facfb35c4bcbe5e51a5
Egor Volg <duna9588@gmail.com>
2025-08-18 17:53:44 +0300

    refactor: refactor package

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -1,19 +1,15 @@
-exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: { 
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleNameMapping: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testMatch: ["**/__tests__/**/*.test.ts", "**/?(*.)+(spec|test).ts"],
-  collectCoverageFrom: [
-    "src/**/*.{ts,js}",
-    "!src/**/*.d.ts",
-    "!src/**/*.test.{ts,js}",
-  ],
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov", "html"],
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
 };
