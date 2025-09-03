5c2dc589aa9c2ea85b12917930a3027f8c29411f
Egor Volg <duna9588@gmail.com>
2025-09-03 20:54:44 +0300

    refactor: moved header

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,11 +1,13 @@
 import { Homepage } from "../pages/home/Homepage";
 import Container from "../shared/ui/Container/Container";
+import Header from "../widgets/Header/Header";
 import styles from "./styles/App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
       <Container className={styles.container}>
+        <Header />
         <Homepage />
       </Container>
     </div>
