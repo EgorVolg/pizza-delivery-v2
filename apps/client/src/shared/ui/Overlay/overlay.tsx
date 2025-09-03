f4fc433d55e2393ba29a01e3aa6a9620d441e00b
Egor Volg <duna9588@gmail.com>
2025-09-03 20:54:25 +0300

    refactor: refactor overlay

--- apps/client/src/shared/ui/Overlay/overlay.tsx
+++ apps/client/src/shared/ui/Overlay/overlay.tsx
@@ -1,5 +1,15 @@
-import styles from "./overlay.module.css";
+import styles from "./Overlay.module.css";
 
-export const Overlay = ({ onClick }: { onClick: () => void }) => {
-  return <div className={styles.overlay} onClick={onClick} />;
+export const Overlay = ({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className={styles.overlay} onClick={onClick}>
+      {children}
+    </div>
+  );
 };
