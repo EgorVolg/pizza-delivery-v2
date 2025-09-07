a0155a7279fef28f035c4aafc432a5a7bb3b153c
Egor Volg <duna9588@gmail.com>
2025-09-07 03:04:20 +0300

    feat: implement framer-motion animations for category selection and remove unused scroll category hook

--- apps/client/src/entities/topbar/categories/ui/Categories.tsx
+++ apps/client/src/entities/topbar/categories/ui/Categories.tsx
@@ -1,24 +1,27 @@
 import styles from "./Categories.module.css";
+import { motion } from "framer-motion";
 import { useGetCategoriesQuery } from "../model/categories.api";
 import { CategoriesSkeleton } from "./Categories.Skeleton";
 import type { Category } from "../model/categories.types";
 import { useDispatch, useSelector } from "react-redux";
+import { setActiveId } from "../model/activeCategories.slice";
 
 export default function CategoriesList() {
   const { data: categories, isLoading } = useGetCategoriesQuery();
   const selectedCategory = useSelector(
     (state: any) => state.setActiveId.activeId
   );
-  const handleSelectCategoryId = useDispatch();
+  const selectCategoryId = useDispatch();
 
-  const handleClick = (
-    event: React.MouseEvent<HTMLAnchorElement>,
-    newCategoryId: number
-  ) => {
+  const handleClick = (event: React.MouseEvent, index: number) => {
     event.preventDefault();
-    handleSelectCategoryId({
-      type: "category/setActiveId",
-      payload: newCategoryId,
+    selectCategoryId(setActiveId(index));
+
+    const target = event.currentTarget as HTMLAnchorElement;
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
     });
   };
 
@@ -26,18 +29,29 @@ export default function CategoriesList() {
     <div className={styles.categories}>
       {isLoading
         ? [...Array(6)].map((_, index) => <CategoriesSkeleton key={index} />)
-        : categories?.map((category: Category, index: number) => (
-            <a
-              key={index}
-              href="#"
-              className={`${styles.category} ${
-                selectedCategory === index + 1 ? styles.active : ""
-              }`}
-              onClick={(event) => handleClick(event, index + 1)}
-            >
-              {category.name}
-            </a>
-          ))}
+        : categories?.map((category: Category, index: number) => {
+            const isActive = selectedCategory === index + 1;
+
+            return (
+              <motion.a
+                key={index}
+                href="#"
+                className={styles.category}
+                onClick={(event) => handleClick(event, index + 1)}
+                animate={{
+                  backgroundColor: isActive ? "#fff" : "transparent",
+                  color: isActive ? "#fe5f00" : "#000",
+                  boxShadow: isActive
+                    ? "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)"
+                    : "0px 0px 0px rgba(0,0,0,0)",
+                }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                style={{ textWrap: "nowrap" }}
+              >
+                {category.name}
+              </motion.a>
+            );
+          })}
     </div>
   );
 }
