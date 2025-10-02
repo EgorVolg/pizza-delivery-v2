fa213156c13d840a51684cdc9cbd23e5929f3955
Egor Volg <duna9588@gmail.com>
2025-10-02 15:22:22 +0300

    feat(cart): add update cart item endpoint and enhance get items response
    - Added `updateCartItem` function to handle PUT requests for updating cart items by ID
    - Introduced PUT `/cart/:id` route for cart item updates
    - Enhanced `getCartItems` to calculate and return total quantity and total price alongside item data for better API response

--- apps/server/src/routes/cart.routes.ts
+++ apps/server/src/routes/cart.routes.ts
@@ -4,7 +4,18 @@ import { Cart } from "../entities/cart/model/cart.model";
 export const getCartItems = async (_req: Request, res: Response) => {
   try {
     const items = await Cart.findAll();
-    res.json(items);
+
+    const quantity = items.reduce((total, item) => total + item.quantity, 0);
+    const totalPrice = items.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+
+    res.json({
+      data: items,
+      quantity,
+      totalPrice: totalPrice,
+    });
   } catch (err) {
     console.error("Ошибка при получении корзины:", err);
     res.status(500).json({ error: "Server error" });
@@ -40,6 +51,24 @@ export const deleteCartItem = async (req: Request, res: Response) => {
   }
 };
 
+export const updateCartItem = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const [updated] = await Cart.update(req.body, {
+      where: { id },
+    });
+
+    if (updated) {
+      res.json({ message: "Товар обновлен в корзине" });
+    } else {
+      res.status(404).json({ error: "Товар не найден" });
+    }
+  } catch (err) {
+    console.error("Ошибка при обновлении товара в корзине:", err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
 const router = express.Router();
 
 // GET /cart — получить все записи
@@ -50,5 +79,8 @@ router.post("/cart", addCartItem);
 
 // DELETE /cart/:id — удалить запись по id
 router.delete("/cart/:id", deleteCartItem);
+
+// PUT /cart/:id — обновить запись по id
+router.put("/cart/:id", updateCartItem);
 
 export default router;
