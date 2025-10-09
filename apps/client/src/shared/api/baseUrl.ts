555c8d9cac2a7c422b6a768fa7a5fcbe3c4e32fa
Egor Volg <duna9588@gmail.com>
2025-10-09 17:58:32 +0300

    refactor(api): implement dynamic base URL for environment-specific API endpoints
    Replaced hardcoded localhost base URL with a `getBaseUrl()` function that returns localhost URL for development and a network IP for other environments, improving flexibility for different deployment scenarios.

--- apps/client/src/shared/api/baseUrl.ts
+++ apps/client/src/shared/api/baseUrl.ts
@@ -1,6 +1,17 @@
 // shared/api/baseQuery.ts
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const getBaseUrl = () => {
+  if (
+    typeof window !== "undefined" &&
+    window.location.hostname === "localhost"
+  ) {
+    return "http://localhost:5000/api";
+  }
+
+  return "http://192.168.0.199:5000/api";
+};
+
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: getBaseUrl(),
 });
