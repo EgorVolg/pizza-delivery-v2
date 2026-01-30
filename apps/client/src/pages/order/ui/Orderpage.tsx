4f5e19f0c6d6a47335cb7f963a4c97a12952b371
Egor Volg <duna9588@gmail.com>
2026-01-30 11:59:13 +0300

    feat(Orderpage): add Orderpage component with styles and layout for order summary

---
+++ apps/client/src/pages/order/ui/Orderpage.tsx
@@ -0,0 +1,64 @@
+import Button from "../../../shared/ui/Button/Button";
+import Container from "../../../shared/ui/Container/Container";
+import track from "../../../shared/assets/track.svg";
+import orderbox from "../../../shared/assets/orderbox.svg";
+import percent from "../../../shared/assets/percent.svg";
+import styles from "./Orderpage.module.css";
+
+export const Orderpage = () => {
+  return (
+    <Container className={styles.orderpage_container}>
+      <div className={styles.orderpage}>
+        <h1 className={styles.title_section}>Оформление заказа</h1>
+      </div>
+
+      <div className={styles.orderpage_spacer}>
+        <div className={styles.left}>Левый блок</div>
+
+        <div className={styles.order_info}>
+          <div className={styles.order_info_section}>
+            <p className={styles.order_info_title}>Итого:</p>
+            <p className={styles.order_info_price}>5 400 ₽</p>
+          </div>
+
+          <div className={styles.order_info_section}>
+            <ul className={styles.order_info_list}>
+              <li className={styles.order_info_listitem}>
+                <span className={styles.order_info_listitem_span}>
+                  <img src={orderbox} alt="total price" /> Стоимость товаров:
+                </span>
+                <div className={styles.order_info_dots} />
+                <p className={styles.order_info_amount}>5 400 ₽</p>
+              </li>
+              <li className={styles.order_info_listitem}>
+                <span className={styles.order_info_listitem_span}>
+                  <img src={percent} alt="tax" />
+                  Налог:
+                </span>
+                <div className={styles.order_info_dots} />
+                <p className={styles.order_info_amount}>5 400 ₽</p>
+              </li>
+              <li className={styles.order_info_listitem}>
+                <span className={styles.order_info_listitem_span}>
+                  <img src={track} alt="delivery" />
+                  Доставка:
+                </span>
+                <div className={styles.order_info_dots} />
+                <p className={styles.order_info_amount}>5 400 ₽</p>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.order_info_section}>
+            <input
+              type="text"
+              placeholder="У меня есть промокод"
+              className={styles.promo_code_input}
+            />
+            <Button className={styles.goPay_button}>Перейти к оплате</Button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
